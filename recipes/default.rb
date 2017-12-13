003b43b617d7f825b1663dcb2f93f09206ca41a0
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-13 11:19:01 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 case node['platform']
 when 'amazon'
-  include_recipe 'web::webpage'
+  include_recipe 'web::linux'
 when 'ubuntu'
-  include_recipe 'web::webpage'
+  include_recipe 'web::ubuntu'
 end
